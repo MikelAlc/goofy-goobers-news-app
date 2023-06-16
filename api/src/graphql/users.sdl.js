6dc6ac755fc4ed3f629da3552d144d4f697cbf1c
POWERUSER <tyresehutcheson@gmail.com>
2023-06-15 20:34:15 -0400

    API key obtained, setting up article retrieval
    Successfully implemented client side article retrieval, now switching over to server-side

---
+++ api/src/graphql/users.sdl.js
@@ -0,0 +1,61 @@
+export const schema = gql`
+  type User {
+    id: Int!
+    name: String!
+    email: String
+    hashedPassword: String!
+    salt: String!
+    resetToken: String
+    resetTokenExpiresAt: DateTime
+    general: Boolean!
+    business: Boolean!
+    entertainment: Boolean!
+    health: Boolean!
+    science: Boolean!
+    sports: Boolean!
+    technology: Boolean!
+  }
+
+  type Query {
+    users: [User!]! @skipAuth
+    user(id: Int!): User @skipAuth
+  }
+
+  input CreateUserInput {
+    name: String!
+    email: String
+    hashedPassword: String!
+    salt: String!
+    resetToken: String
+    resetTokenExpiresAt: DateTime
+    general: Boolean!
+    business: Boolean!
+    entertainment: Boolean!
+    health: Boolean!
+    science: Boolean!
+    sports: Boolean!
+    technology: Boolean!
+  }
+
+  input UpdateUserInput {
+    name: String
+    email: String
+    hashedPassword: String
+    salt: String
+    resetToken: String
+    resetTokenExpiresAt: DateTime
+    general: Boolean
+    business: Boolean
+    entertainment: Boolean
+    health: Boolean
+    science: Boolean
+    sports: Boolean
+    technology: Boolean
+  }
+
+  type Mutation {
+    createUser(input: CreateUserInput!): User! @requireAuth
+    updateUser(id: Int!, input: UpdateUserInput!): User! @requireAuth
+    deleteUser(id: Int!): User! @requireAuth
+  }
+`
---
+++ api/src/services/users/users.scenarios.js
@@ -0,0 +1,10 @@
+export const standard = defineScenario({
+  user: {
+    one: {
+      data: { name: 'String2970382', hashedPassword: 'String', salt: 'String' },
+    },
+    two: {
+      data: { name: 'String8216632', hashedPassword: 'String', salt: 'String' },
+    },
+  },
+})
