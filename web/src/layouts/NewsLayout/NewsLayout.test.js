714b6965920f1e131202db1b49b3398bc3873e08
Mikel Alcazar <mklalcazar@gmail.com>
2023-06-20 05:35:07 -0400

    Added Settings NavBar Tests

--- web/src/layouts/NewsLayout/NewsLayout.test.js
+++ web/src/layouts/NewsLayout/NewsLayout.test.js
@@ -13,6 +13,49 @@ describe('NewsLayout', () => {
     }).not.toThrow()
   })
 
+  it('has settings tab when logged in', async () => {
+    const currentUser= {
+          id: 1,
+          name: 'Tester',
+          general: true,
+          business: false,
+          entertainment: false,
+          health: false,
+          science: false,
+          sports: false,
+          technology: false,
+        }
+
+        mockCurrentUser(currentUser)
+
+        render(<NewsLayout />)
+        expect(screen.getByText('Settings')).toBeInTheDocument()
+  })
+
+  it('has no settings tab when logged out', async () => {
+    mockCurrentUser(null)
+    render(<NewsLayout />)
+    expect(screen.queryByText('Settings')).not.toBeInTheDocument()
+  })
+
+  it('has settings tab when logged in', async () => {
+    const currentUser= {
+          id: 1,
+          name: 'Tester',
+          general: true,
+          business: false,
+          entertainment: false,
+          health: false,
+          science: false,
+          sports: false,
+          technology: false,
+        }
+
+        mockCurrentUser(currentUser)
+
+        render(<NewsLayout />)
+        expect(screen.getByText('Settings')).toBeInTheDocument()
+  })
 
 
 
