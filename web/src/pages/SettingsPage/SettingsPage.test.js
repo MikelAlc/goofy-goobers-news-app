e035c7b1226e9d6c8045f56ffa43616ca545c6a7
Mikel Alcazar <mklalcazar@gmail.com>
2023-06-20 03:17:46 -0400

    Passed SettingsPage default test

--- web/src/pages/SettingsPage/SettingsPage.test.js
+++ web/src/pages/SettingsPage/SettingsPage.test.js
@@ -1,14 +1,32 @@
 import { render } from '@redwoodjs/testing/web'
 
 import SettingsPage from './SettingsPage'
+import { flattenSearchParams } from '@redwoodjs/router/dist/util'
 
 //   Improve this test with help from the Redwood Testing Doc:
 //   https://redwoodjs.com/docs/testing#testing-pages-layouts
 
 describe('SettingsPage', () => {
-  it('renders successfully', () => {
+  it('renders successfully', async () => {
+
+    mockGraphQLQuery('getCurrentUser', () => {
+      return {
+        currentUser: {
+          id: 1,
+          name: 'Tester',
+          general: true,
+          business: false,
+          entertainment: false,
+          health: false,
+          science: false,
+          sports: false,
+          technology: false,
+        }
+      }
+    })
+
     expect(() => {
-      render(<SettingsPage />)
+      render(await <SettingsPage />)
     }).not.toThrow()
   })
 })
