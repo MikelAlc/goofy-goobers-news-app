41902ea0a1e9558897d9bc515f3b329358a29c51
POWERUSER <tyresehutcheson@gmail.com>
2023-06-22 19:44:48 -0400

    Final changes 2

--- web/src/layouts/NewsLayout/NewsLayout.js
+++ web/src/layouts/NewsLayout/NewsLayout.js
@@ -2,7 +2,6 @@ import { Link, routes } from '@redwoodjs/router'
 import { useAuth } from 'src/auth'
 import logo from 'web/public/img/pub_logos.png'
 
-const testVar = 'hi'
 
 const NewsLayout = ({ children }) => {
   const { isAuthenticated, currentUser, logOut } = useAuth()
