4ae5ed11179731b32f8c2ae492afa5f26ff62d9d
Tristan Draper <107201220+tdraper@users.noreply.github.com>
2023-06-29 17:31:08 +0000

    changed search error message

--- web/src/pages/LandingPage/LandingPage.js
+++ web/src/pages/LandingPage/LandingPage.js
@@ -1,33 +1,37 @@
+import { useState } from 'react'
+
+import logo from 'web/public/img/pub_logos.png'
+
+import { Form, TextField, Submit } from '@redwoodjs/forms'
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
+
 import { useAuth } from 'src/auth'
-import { useState } from 'react'
 import ArticlesCell from 'src/components/ArticlesCell'
-import logo from 'web/public/img/pub_logos.png'
-import { Form, TextField, Submit } from '@redwoodjs/forms'
-
-
 
 const LandingPage = () => {
   const { isAuthenticated, currentUser, logOut } = useAuth()
   const [state, changeState] = useState()
 
-
   const onSubmit = (data) => {
-    const hasEvenParentheses = /^([^()]*\([^()]*\))*[^()]*$/.test(input);
+    const hasEvenParentheses = /^([^()]*\([^()]*\))*[^()]*$/.test(input)
     if (!hasEvenParentheses) {
-      console.log('Missing parenthesis. Please enter a valid search query');
-      return;
+      console.log('Invalid Request')
+      return
     }
     console.log(data)
     changeState(data.input)
   }
 
-
-  const setCategory = (event) => { // switch category, select proper tab
+  const setCategory = (event) => {
+    // switch category, select proper tab
     const categories = document.getElementsByClassName('categories')
 
-    for (let i=0; i<categories.length; i++) // reset all links to white
+    for (
+      let i = 0;
+      i < categories.length;
+      i++ // reset all links to white
+    )
       categories[i].style.color = 'white'
     event.target.style.color = '#fa9dec' // set clicked clicked to pink
 
@@ -40,91 +44,131 @@ const LandingPage = () => {
     let prefs = ''
 
     for (const prop in currentUser) {
-      if (typeof(currentUser[prop]) === "boolean" && currentUser[prop])
+      if (typeof currentUser[prop] === 'boolean' && currentUser[prop])
         prefs += prop + '/'
     }
 
     //if(prefs.length==0) return "general"
     return prefs
-
   }
 
-  const setUserPrefs = (event) => { // for home button
+  const setUserPrefs = (event) => {
+    // for home button
     const categories = document.getElementsByClassName('categories')
 
-    for (let i=0; i<categories.length; i++) // reset all links to white
+    for (
+      let i = 0;
+      i < categories.length;
+      i++ // reset all links to white
+    )
       categories[i].style.color = 'white'
     event.target.style.color = '#fa9dec' // set clicked clicked to pink
 
-    if(isAuthenticated)
-      changeState(getUserPrefs())
-    else
-      changeState(null)
+    if (isAuthenticated) changeState(getUserPrefs())
+    else changeState(null)
   }
-
 
   const userPreferences = getUserPrefs()
 
-
   return (
     <>
-
       <header>
         <div className="title">
-            <Link to={routes.landing()}><img id='logo' src={logo} alt="Goofy Goober Logo"></img></Link>
+          <Link to={routes.landing()}>
+            <img id="logo" src={logo} alt="Goofy Goober Logo"></img>
+          </Link>
         </div>
-         <div className='form-container'>
+        <div className="form-container">
           <div>
-            <Form className='form-inline' onSubmit={onSubmit}>
-              <TextField name="input" className='text-field'/>
-              <Submit  className='rw-button rw-button-blue'>Search</Submit>
-           </Form>
+            <Form className="form-inline" onSubmit={onSubmit}>
+              <TextField name="input" className="text-field" />
+              <Submit className="rw-button rw-button-blue">Search</Submit>
+            </Form>
           </div>
           <div className="flex-end">
             {isAuthenticated ? (
               <div className="flex-end">
                 <span>Logged in as {currentUser.name}&nbsp;</span>{' '}
-                <button type="button" onClick={logOut} className='rw-button rw-button-blue'>Logout</button>
+                <button
+                  type="button"
+                  onClick={logOut}
+                  className="rw-button rw-button-blue"
+                >
+                  Logout
+                </button>
               </div>
             ) : (
-              <Link to={routes.login()} className='rw-button rw-button-blue'>Login</Link>
+              <Link to={routes.login()} className="rw-button rw-button-blue">
+                Login
+              </Link>
             )}
           </div>
-
         </div>
         <nav>
           <ul>
             <li>
-              <Link id='home' style={{color: '#fa9dec'}} onClick={setUserPrefs} className='categories'>Home</Link>
+              <Link
+                id="home"
+                style={{ color: '#fa9dec' }}
+                onClick={setUserPrefs}
+                className="categories"
+              >
+                Home
+              </Link>
             </li>
             <li>
-              <Link id='general' className='categories'  onClick={setCategory}>General</Link>
+              <Link id="general" className="categories" onClick={setCategory}>
+                General
+              </Link>
             </li>
             <li>
-                <Link id='business' className='categories'  onClick={setCategory}>Business</Link>
+              <Link id="business" className="categories" onClick={setCategory}>
+                Business
+              </Link>
             </li>
             <li>
-                <Link id='entertainment' className='categories' onClick={setCategory}>Entertainment</Link>
+              <Link
+                id="entertainment"
+                className="categories"
+                onClick={setCategory}
+              >
+                Entertainment
+              </Link>
             </li>
             <li>
-                <Link id='health' className='categories' onClick={setCategory}>Health</Link>
+              <Link id="health" className="categories" onClick={setCategory}>
+                Health
+              </Link>
             </li>
             <li>
-                <Link id='science' className='categories' onClick={setCategory}>Science</Link>
+              <Link id="science" className="categories" onClick={setCategory}>
+                Science
+              </Link>
             </li>
             <li>
-                <Link id='sports' className='categories'  onClick={setCategory}>Sports</Link>
+              <Link id="sports" className="categories" onClick={setCategory}>
+                Sports
+              </Link>
             </li>
             <li>
-                <Link id='technology' className='categories' onClick={setCategory}>Technology</Link>
+              <Link
+                id="technology"
+                className="categories"
+                onClick={setCategory}
+              >
+                Technology
+              </Link>
             </li>
             <li>
-              {isAuthenticated? <Link to={routes.settings()}>Settings</Link>:<></>}
+              {isAuthenticated ? (
+                <Link to={routes.settings()}>Settings</Link>
+              ) : (
+                <></>
+              )}
             </li>
           </ul>
         </nav>
       </header>
-
 
       <MetaTags title="Landing" description="Landing page" />
 
@@ -137,19 +181,19 @@ const LandingPage = () => {
 
       {/*<p>{getUserPrefs()}</p>*/}
 
-      {!state && !isAuthenticated?
+      {!state && !isAuthenticated ? (
         <div>
-          <h2 className="rw-heading rw-heading-secondary">Log in for a custom home feed </h2>
+          <h2 className="rw-heading rw-heading-secondary">
+            Log in for a custom home feed{' '}
+          </h2>
 
-          <img src="img/PA.jpeg" alt="Pugs" width="300px"   />
-
+          <img src="img/PA.jpeg" alt="Pugs" width="300px" />
         </div>
-
-        : (state && <ArticlesCell criteria={state} />) || <ArticlesCell criteria={getUserPrefs()} />
-
-
-      }
-
+      ) : (
+        (state && <ArticlesCell criteria={state} />) || (
+          <ArticlesCell criteria={getUserPrefs()} />
+        )
+      )}
 
       {/* All instances of 'zip' have been changed to 'criteria'.
       This string will determine what articles are shown on the page.
@@ -160,10 +204,6 @@ const LandingPage = () => {
 
       - Ty'rese
         */}
-
-
-
-
     </>
   )
 }
